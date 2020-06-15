2279f24caa5cdcb1c3270d2ca68f0cfbda84085d
manpreetkaur-783 <66217528+manpreetkaur-783@users.noreply.github.com>
2020-06-15 03:07:52 -0400

    Add files via upload

---
+++ FindWay_ManpreetTests/FindWay_ManpreetTests.swift
@@ -0,0 +1,34 @@
+//
+//  FindWay_ManpreetTests.swift
+//  FindWay_ManpreetTests
+//
+//  Created by  user175413 on 14/06/20.
+//  Copyright © 2020  user175413. All rights reserved.
+//
+
+import XCTest
+@testable import FindWay_Manpreet
+
+class FindWay_ManpreetTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
