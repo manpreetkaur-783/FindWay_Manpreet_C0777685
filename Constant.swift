b0ac2c635d7e0341032348564ab8e70c0447d34f
manpreetkaur-783 <66217528+manpreetkaur-783@users.noreply.github.com>
2020-06-15 10:25:47 -0400

    added constant.swift

---
+++ Constant.swift
@@ -0,0 +1,17 @@
+//
+//  Constant.swift
+//  FindWay_Manpreet
+//
+//  Created by  user175413 on 14/06/20.
+//  Copyright © 2020  user175413. All rights reserved.
+//
+
+import UIKit
+
+
+
+
+var AppName = "FindWay_Manpreet_C0777685"
+
+
+
